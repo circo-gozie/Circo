ad9aa0edc7633e4e89e2f02f488e896aa10db6a5
toni-circo <tony@circo.live>
2025-09-05 19:20:41 +0100

    replace image bg url

--- src/components/home/DownloadApp.tsx
+++ src/components/home/DownloadApp.tsx
@@ -3,8 +3,11 @@ import Icon from '../ui/Icon';
 
 const DownloadApp = () => {
   return (
-    <section className="bg-black py-21 px-5">
-      <div className="max-w-screen-xl mx-auto px-8 bg-[url(/images/red-radial-bg.png)] py-21 rounded-[60px] overflow-hidden flex justify-center">
+    <section
+      id="download"
+      className="bg-black py-21 px-5"
+    >
+      <div className="max-w-screen-xl mx-auto px-8 bg-[url(https://assets.circleandclique.com/artifacts/images/red-radial-bg.png)] py-21 rounded-[60px] overflow-hidden flex justify-center">
         <div className="max-w-[600px] mx-auto">
           <h2 className="text-white text-[76px] md:text-[64px] leading-22 md:leading-18 tracking-[-2%] text-center">
             Circo is where you want to be
