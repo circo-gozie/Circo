3142edce12fb62067b43f61d5f785b0dc7744cea
toni-circo <tony@circo.live>
2025-09-06 20:01:42 +0100

    fix: images not displayin

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://assets.circleandclique.com/artifacts/**')]
+    remotePatterns: [new URL('https://assets.circleandclique.com/artifacts/**')],
+    unoptimized: true
   }
 };
 
